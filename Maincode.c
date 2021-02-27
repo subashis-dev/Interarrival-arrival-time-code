60439de9f6b28ac23032365764912b14244b02b5
Subahis Roy Bhowmik <79735184+subashis-dev@users.noreply.github.com>
2021-02-27 11:56:50 +0600

    Update Maincode.c

--- Maincode.c
+++ Maincode.c
@@ -1,38 +1,45 @@
 #include<stdio.h>
-#include <stdlib.h>
 #include<math.h>
 
-int poissonRandom()
-{
-    int n = 0; //counter of iteration
+int poissonRandom(double a) {
+    int n = 0;
     double limit;
-    double x;  //pseudo random number
-    int INT_MAX= 100;
+    double x;
+    int INT_MAX=100;
     limit = exp(-20);
     x = rand() / INT_MAX;
-    while (x >limit)
-    {
+    while (x > limit) {
         n++;
         x *= rand() / INT_MAX;
     }
-
     return n;
 }
 
-    int main()
-    {
-        int g,n,i,interarrival[100],arrival[100];
+int main()
+{
+    int i;
+    int interarrival[20];
+    int arrival[20];
+    poissonRandom(19.0);
 
-        int random[1000];
+    interarrival[0] = 0;
+    arrival[0] = 0;
 
-        arrival[0]=0, random[0] = 0;
-        for(i=1; i<21; i++)
-        {
-            random[i]= poissonRandom(20);
-            arrival[i] = arrival[i-1] + random[i];
-        }
-        for(int c=0; c<20; c++){
-        printf("%d\t %d \t%d\t", c, random[c], arrival[c]);
+    for(i=1;i<20;i++){
+        interarrival[i]= poissonRandom(19.0);
+    }
+
+    for(i=1;i<20;i++){
+
+        arrival[i] = arrival[i-1] + interarrival[i];//arrival time finding using
+    }
+
+    for(int i=0; i<20; i++){
+        printf("%d\t %d \t%d\t", i, interarrival[i], arrival[i]);//print the interarrival time and arrival time
         printf("\n");
         }
-    }
+
+    return 0;
+}
+
+
